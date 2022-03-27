0fbc78af0768835cb411b5f3729f584689664776
bynikitachel <nikitavyatiorec@gmail.com>
2022-03-27 13:10:04 +0300

    added table cell edit

---
+++ src/pages/TablePage/TableRow/TableCell/CellChange/index.js
@@ -0,0 +1,46 @@
+import { useEffect, useRef, useState } from 'react';
+import './index.css';
+
+const CellChange = ({ setIsChanged, cellValue, setCellValue }) => {
+
+    const [newCellValue, setNewCellValue] = useState(cellValue);
+    const textarea = useRef(null);
+
+    useEffect(() => {
+        const onClick = e => textarea.current?.contains(e.target) || setIsChanged(false);
+        document.addEventListener('click', onClick);
+        return () => document.removeEventListener('click', onClick);
+    }, [setIsChanged]);
+
+    const handleCloseChange = () => {
+        changeCellValue(cellValue);
+        setIsChanged(false);
+    }
+
+    const handleOkChange = () => {
+        changeCellValue(newCellValue);
+        setIsChanged(false);
+
+    }
+
+    const changeCellValue = (e) => {
+        setCellValue(e);
+    }
+
+    return (
+        <div className='container-change'>
+            <textarea
+                autoFocus
+                ref={textarea}
+                value={newCellValue}
+                onChange={e => setNewCellValue(e.target.value)}
+            ></textarea>
+            <div className='container-textarea-buttons'>
+                <button className='cell-ok' onClick={() => handleOkChange()}>OK</button>
+                <button className='cell-cancel' onClick={() => handleCloseChange()}>CANCEL</button>
+            </div>
+        </div>
+    )
+}
+
+export default CellChange;
--- src/pages/TablePage/TableRow/TableCell/index.js
+++ src/pages/TablePage/TableRow/TableCell/index.js
@@ -1,7 +1,30 @@
+import { useRef, useState } from 'react';
+import CellChange from './CellChange';
+
 const TableCell = ({ keyI, value }) => {
 
+    const [isChanged, setIsChanged] = useState(false);
+    const [cellValue, setCellValue] = useState(value);
+    const tableTd = useRef(null);
+
+    const changeTd = () => {
+        setIsChanged(true);
+    }
+
+    const cellText = cellValue ? cellValue : value;
+
+    const change = isChanged ?
+        <CellChange
+            tableTd={tableTd}
+            isChanged={isChanged}
+            setIsChanged={setIsChanged}
+            cellValue={cellValue}
+            value={value}
+            setCellValue={setCellValue}
+        /> : cellText;
+
     return (
-        <td key={keyI}>{value}</td>
+        <td key={keyI} ref={tableTd} onDoubleClick={() => changeTd()}>{change}</td>
     )
 }
 
